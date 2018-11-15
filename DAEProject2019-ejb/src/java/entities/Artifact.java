2304c6bbe8a690479a5f110e7fa19287e79a9ad9
oleksandrsopilkov <oleksandrsopilkov@gmail.com>
2018-11-15 20:41:26 +0000

    Artifact
    Nova entidade ARTIFACT

---
+++ DAEProject2019-ejb/src/java/entities/Artifact.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Olek
+ */
+@Entity
+@Table(name = "Artifacts")
+public class Artifact {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "CONFIGURATION_CODE")
+    private Configuration configuration;
+    
+    private String name;
+    
+    
+    private String url;
+    
+    public Artifact(){
+        
+    }
+    public Artifact(long id, String name, String url, Configuration configuration) {
+        this.id = id;
+        this.name = name;
+        this.url = url;
+        this.configuration = configuration;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+    
+    
+    
+    
+}
--- DAEProject2019-ejb/src/java/entities/Configuration.java
+++ DAEProject2019-ejb/src/java/entities/Configuration.java
@@ -6,10 +6,14 @@
 package entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 /**
  *
@@ -22,9 +26,15 @@ public class Configuration implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    
+    @OneToMany(mappedBy = "configuration", cascade = CascadeType.REMOVE)
+    private List<Artifact> artifacts;
 
     public Configuration() {
+        this.artifacts = new ArrayList<>();
     }
+    
+    
 
     
     
